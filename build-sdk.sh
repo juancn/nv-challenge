c88ba9c484004709b9e9afa80d9695090330739a
Juan Cruz Nores <juancn@gmail.com>
2013-07-15 15:32:54 -0300

    Easier

--- build-sdk.sh
+++ build-sdk.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
 cd $(dirname $0)
 rm sdk.zip
 zip -9 sdk.zip $(find -E sdk -follow -not  -regex ".*/(target|\.DS_Store).*" -type file)
+mv sdk.zip ../challenge-app/framework/res/runner/nv/sdk.zip
