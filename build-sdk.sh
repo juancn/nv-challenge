26be1dd88372aa4b121db4e30401959185e746cc
Juan Cruz Nores <juancn@gmail.com>
2013-08-14 13:18:47 -0300

    Now is phase 3

--- build-sdk.sh
+++ build-sdk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 cd $(dirname $0)
-rm sdk2.zip
-zip -9 sdk2.zip $(find -E sdk -follow -not  -regex ".*/(target|\.DS_Store).*" -type file)
-mv sdk2.zip ../challenge-app/framework/res/runner/nv/sdk2.zip
+rm sdk3.zip
+zip -9 sdk3.zip $(find -E sdk -follow -not  -regex ".*/(target|\.DS_Store).*" -type file)
+mv sdk3.zip ../challenge-app/framework/res/runner/nv/sdk3.zip
