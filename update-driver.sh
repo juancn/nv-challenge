387ba6af267426f808be566d7f2bb86164a67908
Juan Cruz Nores <juancn@gmail.com>
2013-08-14 13:18:19 -0300

    Now is phase 3

--- update-driver.sh
+++ update-driver.sh
@@ -2,5 +2,5 @@
 set -x
 cd $(dirname $0)
 mvn clean package
-cp driver/target/driver-2.0.jar ../challenge-app/framework/res/runner/nv/driver-2.0.jar
-cp runner/target/runner-2.0-jar-with-dependencies.jar ../challenge-app/framework/res/runner/nv/runner-2.0-jar-with-dependencies.jar
+cp driver/target/driver-3.0.jar ../challenge-app/framework/res/runner/nv/driver-3.0.jar
+cp runner/target/runner-3.0-jar-with-dependencies.jar ../challenge-app/framework/res/runner/nv/runner-3.0-jar-with-dependencies.jar
